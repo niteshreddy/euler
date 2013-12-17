aa63dedf1a1ee567896ffd1249abfaf6a5a60cff
Nitesh Mudireddy <niteshreddys89@gmail.com>
2013-12-16 19:43:43 -0600

    Problem 1 of Project Euler

---
+++ p1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+def mul3and5():
+    print '''Finding the sum all numbers which are 
+            multiples of 3 or 5.'''
+    sum = 0
+    for x in range(1, 1000):
+        if x%3 == 0:
+            sum = sum + x
+        elif x%5 == 0:
+            sum = sum + x
+        elif x%15 == 0:
+            sum = sum - 15
+
+    print "The sum is:%d"%sum
+
+
+mul3and5()
