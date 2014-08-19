2e6f5991b9b2f43bd2d65956de786568b99a80d4
Nitesh Mudireddy <niteshreddys89@gmail.com>
2014-08-18 20:45:50 -0500

    Problem 14

---
+++ p14.py
@@ -0,0 +1,31 @@
+'''
+	Longest collatz sequence of a number
+	under 1 million.
+'''
+
+import time
+
+start = time.time()
+
+def collatz(n, count=1):
+	while n>1:
+		count += 1
+		if n % 2 == 0:
+			n = n/2
+		else:
+			n = 3*n + 1
+	return count
+
+
+def solution1():
+	max = [0,0]
+	for i in range(1000000):
+		c = collatz(i)
+		if c > max[0]:
+			max[0] = c
+			max[1] = i
+	elapsed = (time.time() - start)
+	type(elapsed)
+	print "Found %s at length %s in %s seconds"%(max[1],max[0],elapsed)
+
+solution1()
